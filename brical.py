467adffb90685fc0495a2dc62ba716c28dbce6fa
ARAKAWA Naoya <naoya.arakawa@nifty.com>
2015-08-08 17:12:25 +0900

    Added exception handling codes.

--- brical.py
+++ brical.py
@@ -5,7 +5,7 @@
 #  Originally licenced for WBAI (wbai.jp) under the Apache License (?)
 #  Created: 2015-07-18
 
-# TODO: submodules, name spaces, subports
+# TODO: name spaces, subports
 
 import sys
 import numpy
@@ -13,9 +13,9 @@ import brica1
 import json
 
 class LanguageInterpreter:
-    __unit_dic={}	# 辞書：BriCA ユニット名⇒ユニットオブジェクト
-    __jsn = None	# json オブジェクト
-    __super_modules={}	# 上位モジュール
+    __unit_dic={}	# Map: BriCA unit name ⇒ unit object
+    __jsn = None	# json object for a BriCA graph
+    __super_modules={}	# Super modules
 
     def __init__(self):
         """ Create a new `LanguageInterpreter` instance.
@@ -56,6 +56,13 @@ class LanguageInterpreter:
           A BriCA agent
 
         """
+	# Checking grounding
+	sub_modules = self.__sub_modules()
+	for unit_key in self.__unit_dic:
+	    if (not unit_key in sub_modules) and (not isinstance(self.__unit_dic[unit_key], brica1.Component)):
+		print >> sys.stderr, "Module " + unit_key + " at the bottom not grounded as a Component!"
+		return
+	# Main logic
 	top_module = brica1.Module()
 	for unit_key in self.__unit_dic.keys():
 	    if not unit_key in self.__super_modules:
@@ -69,6 +76,13 @@ class LanguageInterpreter:
 	agent.add_submodule("__Runtime_Top_Module", top_module)
 	return agent
 
+    def __sub_modules(self):
+	sub_modules = {}
+	for unit in self.__unit_dic:
+	    if unit in self.__super_modules:
+		sub_modules[self.__super_modules[unit]] = unit
+	return sub_modules
+
     def __add_modules(self):
         """ Add modules from the JSON description
         Args:
@@ -81,44 +95,63 @@ class LanguageInterpreter:
             self.__add_a_module(module)
 
     def __add_a_module(self, module):
-    	if "ImplClass" in module:
-    	    # if an implementation class is specified
-    	    print "Use the existing ImplClass " + module["ImplClass"] + " for " + module["Name"] + "."
-    	    try:
-		self.__unit_dic[module["Name"]]=eval(module["ImplClass"]+'()')
-		# New ImplClass instance
-    	    except:
-		print >> sys.stderr, "Component " + module["ImplClass"] + " not found for " +  module["Name"] + "!"
-		self.__unit_dic[module["Name"]]=brica1.Module()	# New Module instance
-	elif module["Name"] in self.__unit_dic:
+	module_name = module["Name"].strip()
+	if module_name == "":
+	    return
+	if "ImplClass" in module:
+	    # if an implementation class is specified
+	    implclass = module["ImplClass"].strip()
+	    if implclass != "":
+		print "Use the existing ImplClass " + implclass + " for " + module_name + "."
+		try:
+		    self.__unit_dic[module_name]=eval(implclass+'()')
+		    # New ImplClass instance
+		except:
+		    print >> sys.stderr, "Component " + implclass + " not found for " +  module_name + "!"
+		    self.__unit_dic[module_name]=brica1.Module()	# New Module instance
+	elif module_name in self.__unit_dic:
 	    pass	# TODO: Merge module properties
 	else:		# No pre-existing unit with the same name
 	    print "Create a new module instance"
-	    self.__unit_dic[module["Name"]]=brica1.Module()		# New Module instance
+	    self.__unit_dic[module_name]=brica1.Module()		# New Module instance
 	if "SuperModule" in module:
-	    if module["SuperModule"].strip() != "":
-		self.__super_modules[module["Name"]]=module["SuperModule"]
-		if module["SuperModule"] in self.__unit_dic:
-		    if isinstance(self.__unit_dic[module["Name"]], brica1.Component):
-			self.__unit_dic[module["SuperModule"]].add_component(module["Name"], self.__unit_dic[module["Name"]])
-			print "Adding a component " + module["Name"] + " to " + module["SuperModule"] + "."
-		    elif isinstance(self.__unit_dic[unit_key], brica1.Module):
-			self.__unit_dic[module["SuperModule"]].add_submodule(module["Name"], self.__unit_dic[module["Name"]])
-			print "Adding a module " + module["Name"] + " to " + module["SuperModule"] + "."
-		else:
-		    print "SuperModule " + module["SuperModule"] + "has not been defined."
+	    supermodule=module["SuperModule"].strip()
+	    if supermodule != "":
+		self.__add_subunit(supermodule,module_name)
 	if "SubModules" in module:
 	    for submodule in module["SubModules"]:
-		self.__super_modules[submodule]=module["Name"]
-		if submodule in self.__unit_dic:
-		    if isinstance(self.__unit_dic[submodule], brica1.Component):
-			self.__unit_dic[module["Name"]].add_component(submodule, self.__unit_dic[submodule])
-			print "Adding a component " + submodule + " to " + module["Name"] + "."
-		    elif isinstance(self.__unit_dic[unit_key], brica1.Module):
-			self.__unit_dic[module["Name"]].add_submodule(submodule, self.__unit_dic[submodule])
-			print "Adding a module " + submodule + " to " + module["Name"] + "."
-		else:
-		    print "SubModule " + submodule + "has not been defined."
+		self.__add_subunit(module_name,submodule) 
+
+    def __add_subunit(self, superunit, subunit):
+	if subunit in self.__super_modules:
+	    print >> sys.stderr, "Cannot add SubModule " + subunit + " twice!"
+	    return
+	if self.__loop_check(superunit, subunit):
+	    print >> sys.stderr, "Loop detected while trying to add " + subunit + " as a subunit to " + superunit + "!"
+	    return
+	self.__super_modules[subunit] = superunit
+	if not subunit in self.__unit_dic:
+	    print >> sys.stderr, "SubModule " + subunit + "has not been defined."
+	    return
+	if not superunit in self.__unit_dic:
+	    print >> sys.stderr, "SuperModule " + superunit + "has not been defined."
+	    return
+	if isinstance(self.__unit_dic[subunit], brica1.Component):
+	    self.__unit_dic[superunit].add_component(subunit, self.__unit_dic[subunit])
+	    print "Adding a component " + subunit + " to " + superunit + "."
+	elif isinstance(self.__unit_dic[unit_key], brica1.Module):
+	    self.__unit_dic[superunit].add_submodule(subunit, self.__unit_dic[subunit])
+	    print "Adding a module " + subunit + " to " + superunit + "."
+
+    def __loop_check(self, superunit, subunit):
+	if superunit == subunit:
+	   return True
+	val = superunit
+	while val in self.__super_modules:
+	    val = self.__super_modules[val]
+	    if val == subunit:
+		return True
+	return False
 
     def __add_ports(self):
         """ Add ports from the JSON description
@@ -135,16 +168,33 @@ class LanguageInterpreter:
             self.__add_a_port(port)
 
     def __add_a_port(self, port):
-	port_module = port["Module"]	# TODO: "Unit"?
-	port_type = port["Type"]
-	port_name = port["Name"]
-	dimension = port["Dimension"]
+	try:
+	    port_module = port["Module"].strip()	# TODO: "Unit"?
+	except KeyError:
+	    print >> sys.stderr, "Module not specified while adding a port!"
+	    return
+	try:
+	    port_type = port["Type"].strip()
+	except KeyError:
+	    print >> sys.stderr, "Type not specified while adding a port!"
+	    return
+	try:
+	    port_name = port["Name"].strip()
+	except KeyError:
+	    print >> sys.stderr, "Name not specified while adding a port!"
+	    return
+	try:
+	    dimension = port["Dimension"]
+	except KeyError:
+	    print >> sys.stderr, "Dimension not specified while adding a port!"
+	    return
 	try:
 	    length = 1
 	    for i in dimension:
 		length=length*i
 	    if length < 1:
-		raise ValueError("Port dimension < 1!")
+		print >> sys.stderr, "Port dimension < 1!"
+		return
 	    if port_module in self.__unit_dic:
 		module=self.__unit_dic[port_module]
 		if port_type == "Input":
@@ -157,8 +207,6 @@ class LanguageInterpreter:
 		    print >> sys.stderr, "Invalid port type!"
 	    else:
 		print >> sys.stderr, "Module " + port_module + " not found!"
-	except IndexError:
-	    print >> sys.stderr, "Dimension error for the port " + port_name + "!"
 	except ValueError:
 	    print >> sys.stderr, "Dimension error for the port " + port_name + "!"
 	except:
@@ -180,65 +228,84 @@ class LanguageInterpreter:
 
     def __add_a_connection(self, connection):
 	# TODO: Port length check
-	connection_name = connection["Name"]
-	from_unit = connection["FromModule"]
-	from_port = connection["FromPort"]
-	to_unit = connection["ToModule"]
-	to_port = connection["ToPort"]
+	try:
+	    connection_name = connection["Name"]
+	except KeyError:
+	    print >> sys.stderr, "Name not specified while adding a connection!"
+	    return
+	try:
+	    from_unit = connection["FromModule"]
+	except KeyError:
+	    print >> sys.stderr, "FromModule not specified while adding a connection!"
+	    return
+	try:
+	    from_port = connection["FromPort"]
+	except KeyError:
+	    print >> sys.stderr, "FromPort not specified while adding a connection!"
+	    return
+	try:
+	    to_unit = connection["ToModule"]
+	except KeyError:
+	    print >> sys.stderr, "ToModule not specified while adding a connection!"
+	    return
+	try:
+	    to_port = connection["ToPort"]
+	except KeyError:
+	    print >> sys.stderr, "ToPort not specified while adding a connection!"
+	    return
 	# Checking if the modules have been defined.
 	if not from_unit in self.__unit_dic:
-	    msg = "Module " + from_unit + " not defined!"
-	    raise KeyError(msg)
+	    print >> sys.stderr, "Module " + from_unit + " not defined!"
 	    return
 	if not to_unit in self.__unit_dic:
-	    msg = "Module " + to_unit + " not defined!"
-	    raise KeyError(msg)
+	    print >> sys.stderr, "Module " + to_unit + " not defined!"
 	    return
 	# if from_unit & to_unit belong to the same level
-	if (not from_unit in self.__super_modules) and (not to_unit in self.__super_modules):
-	    print "No super modules for " + from_unit + " and " + to_unit + "."
-	    
 	if ((not from_unit in self.__super_modules) and (not to_unit in self.__super_modules)) or \
 	(from_unit in self.__super_modules and to_unit in self.__super_modules and (self.__super_modules[from_unit] == self.__super_modules[to_unit])):
 	    try:
-		self.__unit_dic[from_unit].get_out_port(from_port)	# TODO: Name Space
-		self.__unit_dic[to_unit].get_in_port(to_port)		# TODO: Name Space
-	    except KeyError:
+		fr_port_obj = self.__unit_dic[from_unit].get_out_port(from_port)	# TODO: Name Space
+		to_port_obj = self.__unit_dic[to_unit].get_in_port(to_port)		# TODO: Name Space
+		if fr_port_obj.buffer.shape != to_port_obj.buffer.shape:
+		    print >> sys.stderr, "Port dimension unmatch!"
+		    raise Error
+		# Creating a connection
+		brica1.connect((self.__unit_dic[from_unit],from_port), (self.__unit_dic[to_unit],to_port))
+		print "Creating a connection from " + from_port + " of " + from_unit + " to " + to_port + " of " + to_unit + "."
+	    except Error:
 		print >> sys.stderr, "Error adding a connection from " + from_unit + " to " + to_unit + " on the same level but not from an output port to an input port!"
 		return
-	    # Creating a connection
-	    brica1.connect((self.__unit_dic[from_unit],from_port), (self.__unit_dic[to_unit],to_port))
-	    print "Creating a connection from " + from_port + " of " + from_unit + " to " + to_port + " of " + to_unit + "."
 	# else if from_unit is the direct super module of the to_unit
 	elif to_unit in self.__super_modules and self.__super_modules[to_unit]==from_unit:
 	    try:
-		self.__unit_dic[from_unit].get_in_port(from_port)	# TODO: Name Space
-		self.__unit_dic[to_unit].get_in_port(to_port)		# TODO: Name Space
-	    except KeyError:
+		fr_port_obj = self.__unit_dic[from_unit].get_in_port(from_port)		# TODO: Name Space
+		to_port_obj = self.__unit_dic[to_unit].get_in_port(to_port)		# TODO: Name Space
+		if fr_port_obj.buffer.shape != to_port_obj.buffer.shape:
+		    print >> sys.stderr, "Port dimension unmatch!"
+		    raise Error
+		# Creating a connection (alias)
+		self.__unit_dic[to_unit].alias_in_port(self.__unit_dic[from_unit], from_port, to_port)
+		print "Creating a connection (alias) from " + from_port + " of " + from_unit + " to " + to_port + " of " + to_unit + "."
+	    except Error:
 		print >> sys.stderr, "Error adding a connection from the super module " + from_unit + " to " + to_unit + " but not from an input port to an input port!"
 		return
-	    # Creating a connection (alias)
-	    self.__unit_dic[to_unit].alias_in_port(self.__unit_dic[from_unit], from_port, to_port)
-	    print "Creating a connection (alias) from " + from_port + " of " + from_unit + " to " + to_port + " of " + to_unit + "."
 	# else if to_unit is the direct super module of the from_unit
 	elif from_unit in self.__super_modules and self.__super_modules[from_unit]==to_unit:
 	    try:
-		self.__unit_dic[from_unit].get_out_port(from_port)	# TODO: Name Space
-		self.__unit_dic[to_unit].get_out_port(to_port)		# TODO: Name Space
+		fr_port_obj = self.__unit_dic[from_unit].get_out_port(from_port)	# TODO: Name Space
+		to_port_obj = self.__unit_dic[to_unit].get_out_port(to_port)		# TODO: Name Space
+		if fr_port_obj.buffer.shape != to_port_obj.buffer.shape:
+		    print >> sys.stderr, "Port dimension unmatch!"
+		    raise Error
+		# Creating a connection (alias)
+		self.__unit_dic[from_unit].alias_out_port(self.__unit_dic[to_unit], to_port, from_port)
+		print "Creating a connection (alias) from " + from_port + " of " + from_unit + " to " + to_port + " of " + to_unit + "."
 	    except KeyError:
 		print >> sys.stderr, "Error adding a connection from " + from_unit + " to its super module " + to_unit + " but not from an output port to an output port!"
 		return
-	    # Creating a connection (alias)
-	    self.__unit_dic[from_unit].alias_out_port(self.__unit_dic[to_unit], to_port, from_port)
-	    print "Creating a connection (alias) from " + from_port + " of " + from_unit + " to " + to_port + " of " + to_unit + "."
 	# else connection level error!
 	else:
-	    msg = "Trying to add a connection between units " + from_unit + " and " + to_unit + " in a remote level!"
-	    raise KeyError(msg)
+	    print >> sys.stderr, "Trying to add a connection between units " + from_unit + " and " + to_unit + " in a remote level!"
 	    return
 
-# TODO: Link Check
-## All ports are defined?
-## All modules are grounded as components?
-## Port dimensions matches?
 print "BriCAL.py loaded!"
